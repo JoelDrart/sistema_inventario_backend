7f3f742bc4a639cb2ad23be8af17264af2e30c6a
Erick Malan <erickjpk1234@gmail.com>
2025-03-26 11:15:00 -0500

    Elimina el controlador y servicio de la aplicación, y agrega un middleware de registro para monitorear las solicitudes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { DrizzleModule } from './drizzle/drizzle.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [DrizzleModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+      consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
